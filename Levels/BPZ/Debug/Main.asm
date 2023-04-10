08490e76308beb6ab79bc5b4b8b8c4306f7810f9
Ushupiuck <giancarloknd@hotmail.com>
2023-04-10 20:57:23 -0300

    Update Main.asm

--- Levels/BPZ/Debug/Main.asm
+++ Levels/BPZ/Debug/Main.asm
@@ -2,4 +2,3 @@
 	dbglistobj Obj_Ring,			Map_Ring,		   0,   0, make_art_tile($3D2,3,1)
 	dbglistobj Obj_PathSwap,		Map_PathSwap,		   9,   1, make_art_tile($3D2,3,0)
 	dbglistobj Obj_PathSwap,		Map_PathSwap,		  $D,   5, make_art_tile($3D2,3,0)
-	dbglistobj Obj_BPZElephantBlock,	Map_BPZElephantBlock,	   0,   0, make_art_tile($300,0,0)
