4d2ea166abf9d466b3c8b87b363a44541e65d279
Ushupiuck <giancarloknd@hotmail.com>
2023-04-13 01:53:16 -0300

    Create Map - S1 Shield.asm

---
+++ General/Sprites/Shields/Map - S1 Shield.asm
@@ -0,0 +1,21 @@
+Map_80ba_0: 	dc.w Map_80ba_8-Map_S1Shield
+Map_80ba_2: 	dc.w Map_80ba_A-Map_S1Shield
+Map_80ba_4: 	dc.w Map_80ba_24-Map_S1Shield
+Map_80ba_6: 	dc.w Map_80ba_3E-Map_S1Shield
+Map_80ba_8: 	dc.b $0, $0
+Map_80ba_A: 	dc.b $0, $4
+	dc.b $E8, $A, $0, $0, $FF, $E8
+	dc.b $E8, $A, $0, $9, $0, $0
+	dc.b $0, $A, $10, $0, $FF, $E8
+	dc.b $0, $A, $10, $9, $0, $0
+Map_80ba_24: 	dc.b $0, $4
+	dc.b $E8, $A, $8, $0, $FF, $E9
+	dc.b $E8, $A, $0, $0, $0, $0
+	dc.b $0, $A, $18, $0, $FF, $E9
+	dc.b $0, $A, $10, $0, $0, $0
+Map_80ba_3E: 	dc.b $0, $4
+	dc.b $E8, $A, $8, $0, $FF, $E8
+	dc.b $E8, $A, $8, $9, $0, $0
+	dc.b $0, $A, $18, $0, $FF, $E8
+	dc.b $0, $A, $18, $9, $0, $0
+	even
