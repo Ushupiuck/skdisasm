73e9e1bc9b574cf15ef681f30a7bbff0679554a1
Ushupiuck <giancarloknd@hotmail.com>
2023-04-13 01:56:52 -0300

    Create DPLC - Classic Shield.asm

---
+++ General/Sprites/Shields/DPLC - Classic Shield.asm
@@ -0,0 +1,14 @@
+DPLC_313f_0: 	dc.w DPLC_313f_8-DPLC_ClassicShield
+DPLC_313f_2: 	dc.w DPLC_313f_A-DPLC_ClassicShield
+DPLC_313f_4: 	dc.w DPLC_313f_10-DPLC_ClassicShield
+DPLC_313f_6: 	dc.w DPLC_313f_14-DPLC_ClassicShield
+DPLC_313f_8: 	dc.b $0, $0
+DPLC_313f_A: 	dc.b $0, $2
+	dc.b $F0, $0
+	dc.b $10, $10
+DPLC_313f_10: 	dc.b $0, $1
+	dc.b $80, $12
+DPLC_313f_14: 	dc.b $0, $2
+	dc.b $80, $9
+	dc.b $80, $0
+	even
