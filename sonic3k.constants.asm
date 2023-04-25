105d94c6ab16f988e26c5e4234d1f602c99f9458
Ushupiuck <giancarloknd@hotmail.com>
2023-04-25 04:23:03 -0300

    convert collision to interleaved format

--- sonic3k.constants.asm
+++ sonic3k.constants.asm
@@ -698,17 +698,14 @@ _unkFAC8			ds.w 1
 _unkFACC			ds.b 1
 _unkFACD			ds.b 1
 Pal_fade_delay2			ds.w 1			; timer for palette fade from white routine
-			ds.b $A				; unused
 Palette_rotation_custom		ds.l 1			; custom routine for palette rotation scripts
 Palette_rotation_data		ds.w 9			; data used by palette rotation scripts. Last word must always be 0
-SSZ_MTZ_boss_X_pos			ds.w 1			; horizontal position of the Metropolis Zone boss in Sky Sanctuary
-			ds.w 1				; unused
-SSZ_MTZ_boss_Y_pos			ds.w 1			; vertical position of the Metropolis Zone boss in Sky Sanctuary
-			ds.w 1				; unused
-SSZ_MTZ_boss_X_vel			ds.w 1				; horizontal velocity of the Metropolis Zone boss in Sky Sanctuary
-SSZ_MTZ_boss_Y_vel			ds.w 1				; vertical velocity of the Metropolis Zone boss in Sky Sanctuary
-SSZ_MTZ_boss_laser_timer			ds.w 1	; counts down until a laser is fired in Sky Sanctuary's Metropolis boss fight
-			ds.w 1				; unused
+SSZ_MTZ_boss_X_pos		ds.w 1			; horizontal position of the Metropolis Zone boss in Sky Sanctuary
+SSZ_MTZ_boss_Y_pos		ds.w 1			; vertical position of the Metropolis Zone boss in Sky Sanctuary
+SSZ_MTZ_boss_X_vel		ds.w 1			; horizontal velocity of the Metropolis Zone boss in Sky Sanctuary
+SSZ_MTZ_boss_Y_vel		ds.w 1			; vertical velocity of the Metropolis Zone boss in Sky Sanctuary
+SSZ_MTZ_boss_laser_timer	ds.w 1			; counts down until a laser is fired in Sky Sanctuary's Metropolis boss fight
+			ds.b $10			; unused
 
 DMA_queue			ds.w $12*7		; stores all the VDP commands necessary to initiate a DMA transfer
 DMA_queue_slot			ds.w 1			; points to the next free slot on the queue
