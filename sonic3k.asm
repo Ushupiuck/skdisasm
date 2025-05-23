ee68987b25584cf8487a5309e2a836ac82ff621e
Ushupiuck <giancarloknd@hotmail.com>
2025-05-23 03:47:51 -0300

    Fixed a small derp from a previous build
    That BMI was pointing at the wrong place, completely breaking Sonic's animations.

--- sonic3k.asm
+++ sonic3k.asm
@@ -18921,8 +18921,8 @@ SAnim_Do:
 		add.w	d0,d0
 		adda.w	(a1,d0.w),a1
 		move.b	(a1),d0
-		bmi.s	SAnim_WalkRun
-		move.b	$2A(a0),d1
+		bmi.s	loc_126A4
+		move.b	status(a0),d1
 		andi.b	#1,d1
 		andi.b	#-4,4(a0)
 		or.b	d1,4(a0)
@@ -18934,12 +18934,13 @@ SAnim_Do:
 
 loc_1265A:
 		moveq	#0,d1
-		move.b	$23(a0),d1	; load current frame number
-		move.b	1(a1,d1.w),d0	; read sprite number from script
-		beq.s	SAnim_Next	; If it's a frame ID, branch
+		move.b	anim_frame(a0),d1	; load current frame number
+		move.b	1(a1,d1.w),d0		; read sprite number from script
+		beq.s	SAnim_Next		; If it's a frame ID, branch
 		bpl.s	SAnim_Next
-		cmpi.b	#$FD,d0		; is it a flag from FD to FF?
-		bge.s	SAnim_End_FF	; MJ: if so, branch to flag routines
+		cmpi.b	#$FD,d0			; is it a flag from FC to FF?
+;		bhs.s	SAnim_End_FF		; MJ: if so, branch to flag routines
+		bge.s	SAnim_End_FF		; MJ: if so, branch to flag routines
 
 SAnim_Next:
 		move.b	d0,$22(a0)	; load sprite number
@@ -18996,7 +18997,7 @@ loc_126A4:
 		bpl.s	+
 		moveq	#3,d1
 +
-		andi.b	#-4,4(a0)
+		andi.b	#$FC,render_flags(a0)
 		eor.b	d1,d2
 		or.b	d2,4(a0)
 		btst	#5,$2A(a0)
