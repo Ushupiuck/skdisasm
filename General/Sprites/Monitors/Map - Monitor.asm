8755424e5877588bfa5e3f9ce827710cad20d815
Ushupiuck <giancarloknd@hotmail.com>
2025-05-29 03:12:38 -0300

    Update Map - Monitor.asm

--- General/Sprites/Monitors/Map - Monitor.asm
+++ General/Sprites/Monitors/Map - Monitor.asm
@@ -1,65 +1,78 @@
 Map_a278:
-Map_a278_0: 	dc.w Map_a278_1A-Map_a278
-Map_a278_2: 	dc.w Map_a278_28-Map_a278
-Map_a278_4: 	dc.w Map_a278_3C-Map_a278
-Map_a278_6: 	dc.w Map_a278_50-Map_a278
-Map_a278_8: 	dc.w Map_a278_64-Map_a278
-Map_a278_A: 	dc.w Map_a278_78-Map_a278
-Map_a278_C: 	dc.w Map_a278_8C-Map_a278
-Map_a278_E: 	dc.w Map_a278_A0-Map_a278
-Map_a278_10: 	dc.w Map_a278_B4-Map_a278
-Map_a278_12: 	dc.w Map_a278_C8-Map_a278
-Map_a278_14: 	dc.w Map_a278_DC-Map_a278
-Map_a278_16: 	dc.w Map_a278_F0-Map_a278
-Map_a278_18: 	dc.w Map_a278_104-Map_a278
-Map_a278_1A: 	dc.b $0, $2
+	dc.w Map_a278_1A-Map_a278
+	dc.w Map_a278_28-Map_a278
+	dc.w Map_a278_3C-Map_a278
+	dc.w Map_a278_50-Map_a278
+	dc.w Map_a278_64-Map_a278
+	dc.w Map_a278_78-Map_a278
+	dc.w Map_a278_8C-Map_a278
+	dc.w Map_a278_A0-Map_a278
+	dc.w Map_a278_B4-Map_a278
+	dc.w Map_a278_C8-Map_a278
+	dc.w Map_a278_DC-Map_a278
+	dc.w Map_a278_F0-Map_a278
+	dc.w Map_a278_104-Map_a278
+Map_a278_1A:
+	dc.b $0, $2
 	dc.b $F0, $E, $0, $0, $FF, $F0
 	dc.b $8, $C, $0, $C, $FF, $F0
-Map_a278_28: 	dc.b $0, $3
+Map_a278_28:
+	dc.b $0, $3
 	dc.b $F3, $5, $0, $10, $FF, $F8
 	dc.b $F0, $E, $0, $0, $FF, $F0
 	dc.b $8, $C, $0, $C, $FF, $F0
-Map_a278_3C: 	dc.b $0, $3
+Map_a278_3C:
+	dc.b $0, $3
 	dc.b $F3, $5, $3, $10, $FF, $F8
 	dc.b $F0, $E, $0, $0, $FF, $F0
 	dc.b $8, $C, $0, $C, $FF, $F0
-Map_a278_50: 	dc.b $0, $3
+Map_a278_50:
+	dc.b $0, $3
 	dc.b $F3, $5, $0, $14, $FF, $F8
 	dc.b $F0, $E, $0, $0, $FF, $F0
 	dc.b $8, $C, $0, $C, $FF, $F0
-Map_a278_64: 	dc.b $0, $3
+Map_a278_64:
+	dc.b $0, $3
 	dc.b $F3, $5, $20, $18, $FF, $F8
 	dc.b $F0, $E, $0, $0, $FF, $F0
 	dc.b $8, $C, $0, $C, $FF, $F0
-Map_a278_78: 	dc.b $0, $3
+Map_a278_78:
+	dc.b $0, $3
 	dc.b $F3, $5, $0, $1C, $FF, $F8
 	dc.b $F0, $E, $0, $0, $FF, $F0
 	dc.b $8, $C, $0, $C, $FF, $F0
-Map_a278_8C: 	dc.b $0, $3
+Map_a278_8C:
+	dc.b $0, $3
 	dc.b $F3, $5, $0, $20, $FF, $F8
 	dc.b $F0, $E, $0, $0, $FF, $F0
 	dc.b $8, $C, $0, $C, $FF, $F0
-Map_a278_A0: 	dc.b $0, $3
+Map_a278_A0:
+	dc.b $0, $3
 	dc.b $F3, $5, $0, $24, $FF, $F8
 	dc.b $F0, $E, $0, $0, $FF, $F0
 	dc.b $8, $C, $0, $C, $FF, $F0
-Map_a278_B4: 	dc.b $0, $3
+Map_a278_B4:
+	dc.b $0, $3
 	dc.b $F3, $5, $0, $28, $FF, $F8
 	dc.b $F0, $E, $0, $0, $FF, $F0
 	dc.b $8, $C, $0, $C, $FF, $F0
-Map_a278_C8: 	dc.b $0, $3
+Map_a278_C8:
+	dc.b $0, $3
 	dc.b $F3, $5, $0, $2C, $FF, $F8
 	dc.b $F0, $E, $0, $0, $FF, $F0
 	dc.b $8, $C, $0, $C, $FF, $F0
-Map_a278_DC: 	dc.b $0, $3
+Map_a278_DC:
+	dc.b $0, $3
 	dc.b $F3, $5, $0, $30, $FF, $F8
 	dc.b $F0, $E, $0, $0, $FF, $F0
 	dc.b $8, $C, $0, $C, $FF, $F0
-Map_a278_F0: 	dc.b $0, $3
+Map_a278_F0:
+	dc.b $0, $3
 	dc.b $F3, $5, $0, $34, $FF, $F8
 	dc.b $F0, $E, $0, $0, $FF, $F0
 	dc.b $8, $C, $0, $C, $FF, $F0
-Map_a278_104: 	dc.b $0, $2
+Map_a278_104:
+	dc.b $0, $2
 	dc.b $0, $C, $0, $38, $FF, $F0
 	dc.b $8, $C, $0, $C, $FF, $F0
 	even
